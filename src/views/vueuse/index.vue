bf2a6b5c8c51116b1575d18aa3bff1f033214cdd
liulihao <551772063@qq.com>
2026-01-06 11:49:25 +0800

    fix: 更新useDraggable

--- src/views/vueuse/index.vue
+++ src/views/vueuse/index.vue
@@ -9,8 +9,22 @@ import UseMagicKeys from '@/views/vueuse/useMagicKeys.vue'
 import UseMouseThrottle from '@/views/vueuse/useMouseThrottle.vue'
 import UseFullScreen from '@/views/vueuse/useFullScreen.vue'
 import UseClipboard from '@/views/vueuse/useClipboard.vue'
+import Base from '@/views/vueuse/useDraggable/base.vue'
+import Advanced from '@/views/vueuse/useDraggable/advanced.vue'
 
 const cc = ref()
+const navValue = ref('base')
+
+const options = [
+  {
+    label: '基础',
+    value: 'base',
+  },
+  {
+    label: 'useDraggable',
+    value: 'useDraggable',
+  },
+]
 
 watch(
   cc,
@@ -25,14 +39,23 @@ watch(
 
 <template>
   <div>
-    <useMouse />
-    <UseMouseThrottle />
-    <UseVModels v-model="cc" placeholder="双向数据绑定" />
-    {{ cc }}
-    <OnClickoutside />
-    <UseTitle />
-    <UseMagicKeys />
-    <UseFullScreen />
-    <UseClipboard />
+    <o-tabs v-model="navValue" :options="options">
+      <template #base>
+        <useMouse />
+        <UseMouseThrottle />
+        <UseVModels v-model="cc" placeholder="双向数据绑定" />
+        {{ cc }}
+        <OnClickoutside />
+        <UseTitle />
+        <UseMagicKeys />
+        <UseFullScreen />
+        <UseClipboard />
+      </template>
+
+      <template #useDraggable>
+        <Base />
+        <Advanced />
+      </template>
+    </o-tabs>
   </div>
 </template>
---
+++ src/views/vueuse/useDraggable/base.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useDraggable } from '@vueuse/core'
+import { useTemplateRef } from 'vue'
+console.log(`27 useTemplateRef`, useTemplateRef)
+
+const el = useTemplateRef('el')
+
+// `style` will be a helper computed for `left: ?px; top: ?px;`
+const { x, y, style } = useDraggable(el, {
+  initialValue: { x: 400, y: 400 },
+})
+</script>
+
+<template>
+  <div ref="el" :style="style" style="position: fixed" class="w-200 h-200 bg-white f-ct-ct w">
+    <div>Drag me! at</div>
+    <div>{{ x.toFixed(2) }}, {{ y.toFixed(2) }}</div>
+  </div>
+</template>
