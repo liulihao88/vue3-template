2a5559074269fb7294e603bce9706a19424901dc
liulihao <551772063@qq.com>
2026-01-06 10:16:14 +0800

    fix: 外部点击

--- src/views/vueuse/onClickoutside.vue
+++ src/views/vueuse/onClickoutside.vue
@@ -11,12 +11,16 @@ onClickOutside(containerRef, () => {
   isClickout.value = true
   proxy.$toast('我在区域外点击')
 })
+const clickInner = () => {
+  isClickout.value = false
+  proxy.$toast('内部点击')
+}
 </script>
 
 <template>
   <o-title title="onClickOutside" />
   <div class="w-300 h-300 f-ct-ct">
-    <div ref="containerRef" class="w-100 h-100 bg-blue" :class="isClickout && 'cl-red'" @click="isClickout = false">
+    <div ref="containerRef" class="w-100 h-100 bg-blue" :class="isClickout && 'cl-red'" @click="clickInner">
       我是区域内
     </div>
   </div>
--- src/views/vueuse/useFullscreen.vue
+++ src/views/vueuse/useFullscreen.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import { useFullscreen, onMounted } from '@vueuse/core'
-import { useTemplateRef, ref } from 'vue'
-
+import { ref } from 'vue'
 const el = ref(null)
-console.log(`79 el`, el)
-const { toggle } = useFullscreen(el.value)
+const { toggle, isFullscreen } = useFullscreen(el.value)
 </script>
 
 <template>
   <o-title title="useFullscreen" />
-  <div class="text-center">
-    <div class="flex" p="y-4">
-      <video ref="el" class="m-auto rounded" src="https://vjs.zencdn.net/v/oceans.mp4" width="600" controls />
-    </div>
-    <el-button @click="toggle">点击按钮切换全屏</el-button>
-  </div>
+  <o-flex gap="16">
+    <video ref="el" class="" src="https://vjs.zencdn.net/v/oceans.mp4" width="300" height="300" controls />
+    <el-button type="primary" @click="toggle">{{ isFullscreen ? '退出全屏' : '到全屏' }}</el-button>
+  </o-flex>
 </template>
